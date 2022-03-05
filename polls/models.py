b5e143c4f33d47b69012a7fbd67c1d98ce4466c0
zenna <97446606+zenna9@users.noreply.github.com>
2022-03-06 00:07:57 +0900

    sql 연결했당 ^0^
    근데 url..

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,41 @@
 from django.db import models
 
-from django.db import models
 # 위 구문은 삭제하면 안됨.
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    free-type = models.CharField(max_length=200)
+    area = models.CharField(max_length=20)
+    item_code = models.CharField(max_length=20)
+    target2_code = models.CharField(max_length=50)
+    addr2 = models.CharField(max_length=200)
+    lng = models.CharField(max_length=200)
+    addr1 = models.CharField(max_length=200)
+    open_info = models.CharField(max_length=200)
+    target2_etc = models.CharField(max_length=200)
+    type = models.IntegerField(default=0)
+    close_info = models.CharField(max_length=200)
+    zipcode = models.IntegerField(default=0)
+    img_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    target1_code = models.CharField(max_length=200)
+    idx = models.CharField(max_length=50)
+    item_etc = models.CharField(max_length=200)
+    lat = models.CharField(max_length=200)
+
 
     def __str__(self):
-        return self.question_text
-        # 이 함수는.. question_text에 문자열을 제대로 표현하기 위해 함수를 사용
+        return self.area
+        return self.item_code
+        return self.target2_code
+        return self.addr2
+        return self.lng
+        return self.addr1
+        return self.open_info
+        return self.target2_etc
+        return self.type
+        return self.close_info
+        return self.zipcode
+        return self.img_name
+        return self.name
+        return self.target1_code
+        return self.idx
+        return self.item_etc
+        return self.lat
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    print('안녕하세요, 호출되었습니다! ')
