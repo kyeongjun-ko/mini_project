aa4e228e093f0eaaef13a9566a7852141462028a
zenna <97446606+zenna9@users.noreply.github.com>
2022-03-08 12:58:57 +0900

    다시시작하자 오히려좋아~
    오히려좋아~

--- polls/models.py
+++ polls/models.py
@@ -1,150 +1,49 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
+class StoreDB(models.Model):
+    idx = models.IntegerField(default=0)
+    area = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    addr1 = models.CharField(max_length=200)
+    addr2 = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=100)
+    open_info = models.CharField(max_length=100)
+    close_info = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    target1_code = models.CharField(max_length=100)
+    target1_etc = models.CharField(max_length=100)
+    target2_code = models.CharField(max_length=100)
+    target2_etc = models.CharField(max_length=100)
+    item_code = models.CharField(max_length=100)
+    item_etc = models.CharField(max_length=100)
+    lat = models.CharField(max_length=100)
+    lng = models.CharField(max_length=100)
+    img_name = models.CharField(max_length=100)
+    sup_obj = models.CharField(max_length=100)
+    sup_num = models.CharField(max_length=100)
+    sup_item = models.CharField(max_length=100)
+    img_src = models.CharField(max_length=200)
 
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.IntegerField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
-
-
-class StoresDb(models.Model):
-    area = models.TextField(blank=True, null=True)
-    item_code = models.FloatField(blank=True, null=True)
-    target2_code = models.TextField(blank=True, null=True)
-    addr2 = models.TextField(blank=True, null=True)
-    lng = models.FloatField(blank=True, null=True)
-    addr1 = models.TextField(blank=True, null=True)
-    open_info = models.TextField(blank=True, null=True)
-    target2_etc = models.TextField(blank=True, null=True)
-    type = models.BigIntegerField(blank=True, null=True)
-    close_info = models.TextField(blank=True, null=True)
-    zipcode = models.BigIntegerField(blank=True, null=True)
-    img_name = models.TextField(blank=True, null=True)
-    name = models.TextField(blank=True, null=True)
-    target1_code = models.FloatField(blank=True, null=True)
-    target1_etc = models.TextField(blank=True, null=True)
-    idx = models.BigIntegerField(blank=True, null=True)
-    item_etc = models.TextField(blank=True, null=True)
-    lat = models.FloatField(blank=True, null=True)
-    man1 = models.TextField(blank=True, null=True)
-    man2 = models.TextField(blank=True, null=True)
-    pro = models.TextField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'stores_db'
+    def __str__(self):
+        return self.idx
+        return self.area
+        return self.name
+        return self.addr1
+        return self.addr2
+        return self.zipcode
+        return self.open_info
+        return self.close_info
+        return self.type
+        return self.target1_code
+        return self.target1_etc
+        return self.target2_code
+        return self.target2_etc
+        return self.item_code
+        return self.item_etc
+        return self.lat
+        return self.lng
+        return self.img_name
+        return self.sup_obj
+        return self.sup_num
+        return self.sup_item
+        return self.img_src
